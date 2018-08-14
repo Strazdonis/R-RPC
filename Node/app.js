df101977c953c9bfe887e3e6b8cfe2a0ee57a08f
Edvinas Strazdonis <edvinasstrazdonis@gmail.com>
2018-08-14 17:59:18 +0300

    Version 4
    Added settings UI, more functionality & bug fixes/cleanup.

--- Node/app.js
+++ Node/app.js
@@ -1,13 +1,27 @@
-const pidusage = require('pidusage');
-const find = require('find-process');
+const WebSocket = require("WS");
 const client = require('discord-rich-presence')('475313648500211739');
-var WebSocket = require("WS");
+const fs = require('fs');
 var reconnect;
-var BreakException = {};
+var Updated = 0;
 
-var fs = require('fs');
-if (!fs.existsSync(process.env.HOME + "\\AppData\\Roaming\\Rainmeter\\Plugins\\MessagePassing.dll") || !fs.existsSync(process.env.HOME + "\\AppData\\Roaming\\Rainmeter\\Plugins\\messagepassing.dll")) {
-  console.log("missing MessagePassing.dll");
+if (!fs.existsSync(process.env.APPDATA + "\\Rainmeter\\Plugins\\MessagePassing.dll") || !fs.existsSync(process.env.APPDATA + "\\Rainmeter\\Plugins\\messagepassing.dll")) {
+  console.log("missing MessagePassing.dll (looked for "+process.env.APPDATA +"\\Rainmeter\\Plugins\\MessagePassing.dll OR "+process.env.APPDATA+"\\Rainmeter\\Plugins\\messagepassing.dll)");
+}
+
+function humanFileSize(bytes, si) {
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si
+        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
 }
 
 function open() {
@@ -36,22 +50,45 @@ var onClose = function() {
     }, 5000);
 };
 var onMessage = function(event) {
-  var res = event.data.split(" ");
+var res = event.data.split(" ");
+field = [];
+for(i = 0; i<=1; i++) {
+  if(res[i].includes("CPU")) {
+    worker=res[i].split("CPU").pop();
+    field[i]=Math.round(worker * 10) / 10 + '% CPU usage';
+  } else if(res[i].includes("RAM")) {
+    worker=res[i].split("RAM").pop();
+    field[i]=humanFileSize(worker, true) + ' RAM usage';
+  } else if(res[i].includes("Loaded")) {
+    worker=res[i].split("Loaded").pop();
+    field[i]=worker + ' skins loaded';
+  }
+}
+if(Updated == 0) {
+  if(res[2] == 1) {
+    Updated=1;
+    elapsed=(Date.now( )/ 1000 | 0);
+  } else {
+    elapsed=null;
+  }
+}
+if(res[3] < 6) {
+  image="rainmeter"+res[3];
+} else {
+  image="rainmeter1";
+}
   client.updatePresence({
-    state:  Math.round(res[0] * 10) / 10 + '% CPU usage',
-    details: res[1] + ' skins loaded',
-    largeImageKey: 'rainmeter',
+    state:  field[0],
+    details: field[1],
+    startTimestamp: elapsed,
+    largeImageKey: image,
     largeImageText: 'Desktop customization tool'
   });
+
 };
 var onError = function(event) {
     if (typeof event.data != 'undefined') {
         console.log("Websocket Error:" + event.data);
     }
 };
-function sendMessage(stringToSend) {
-    if (connected) {
-        ws.send(stringToSend);
-    }
-}
 open();
